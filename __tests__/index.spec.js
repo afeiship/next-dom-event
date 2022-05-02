203a4830f1fc48addc2ffb8d0cc9918bca9eb806
afeiship <1290657123@qq.com>
2022-05-03 06:48:35 +0800

    test: add unittest

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,10 +1,25 @@
-(function() {
+(function () {
   const NxDomEvent = require('../src');
 
-  describe('NxDomEvent.methods', function() {
-    test('init', function() {
-      const data = { key: 1, value: 2 };
-      expect(!!data).toBe(true);
+  describe('NxDomEvent.methods', function () {
+    test('exist: on function should exist', function () {
+      expect(typeof NxDomEvent.on).toBe('function');
+    });
+
+    test('on: on a handler, can be trigger', () => {
+      const handler = jest.fn();
+      NxDomEvent.on(document, 'click', handler);
+      document.dispatchEvent(new Event('click'));
+      expect(handler).toHaveBeenCalled();
+    });
+
+    test('off: on a handler, when destroy execute, can not be trigger', () => {
+      const handler = jest.fn();
+      const res = NxDomEvent.on(document, 'click', handler);
+      expect(typeof res.destroy).toBe('function');
+      res.destroy();
+      document.dispatchEvent(new Event('click'));
+      expect(handler).not.toHaveBeenCalled();
     });
   });
 })();
