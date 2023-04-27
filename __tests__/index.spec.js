41bee2869866b855760e85bdfecdb9b5f909779e
afeiship <1290657123@qq.com>
2023-04-27 15:01:26 +0800

    refactor: use nx.event

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,25 +1,23 @@
-(function () {
-  const NxDomEvent = require('../src');
+const NxDomEvent = require('../src');
 
-  describe('NxDomEvent.methods', function () {
-    test('exist: on function should exist', function () {
-      expect(typeof NxDomEvent.on).toBe('function');
-    });
-
-    test('on: on a handler, can be trigger', () => {
-      const handler = jest.fn();
-      NxDomEvent.on(document, 'click', handler);
-      document.dispatchEvent(new Event('click'));
-      expect(handler).toHaveBeenCalled();
-    });
-
-    test('off: on a handler, when destroy execute, can not be trigger', () => {
-      const handler = jest.fn();
-      const res = NxDomEvent.on(document, 'click', handler);
-      expect(typeof res.destroy).toBe('function');
-      res.destroy();
-      document.dispatchEvent(new Event('click'));
-      expect(handler).not.toHaveBeenCalled();
-    });
+describe('NxDomEvent.methods', function () {
+  test('exist: on function should exist', function () {
+    expect(typeof NxDomEvent.on).toBe('function');
   });
-})();
+
+  test('on: on a handler, can be trigger', () => {
+    const handler = jest.fn();
+    NxDomEvent.on(document, 'click', handler);
+    document.dispatchEvent(new Event('click'));
+    expect(handler).toHaveBeenCalled();
+  });
+
+  test('off: on a handler, when destroy execute, can not be trigger', () => {
+    const handler = jest.fn();
+    const res = NxDomEvent.on(document, 'click', handler);
+    expect(typeof res.destroy).toBe('function');
+    res.destroy();
+    document.dispatchEvent(new Event('click'));
+    expect(handler).not.toHaveBeenCalled();
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,67 +1,67 @@
-(function() {
-  var global = global || this || window || Function('return this')();
-  var nx = global.nx || require('@jswork/next');
-  var document = global.document;
-  var FUNCTION = 'function';
+import nx from '@jswork/next';
 
-  var addEventListener = (function () {
-    if (document.addEventListener) {
-      return function (inElement, inName, inCallback, inCapture) {
-        inElement.addEventListener(inName, inCallback, inCapture || false);
-      };
-    } else if (document.attachEvent) {
-      return function (inElement, inName, inCallback) {
-        var name = 'on' + inName;
-        inElement.attachEvent(name, function (e) {
-          e = e || global.event;
-          e.target = e.target || e.srcElement;
-          e.currentTarget = node;
-          inCallback.call(node, e);
-        });
-      };
-    }
-  })();
+const document = global.document;
+const FUNCTION = 'function';
 
-  var removeEventListener = (function () {
-    if (document.removeEventListener) {
-      return function (inElement, inName, inCallback, inCapture) {
-        inElement.removeEventListener(inName, inCallback, inCapture || false);
-      };
-    } else if (document.detachEvent) {
-      return function (inElement, inName, inCallback) {
-        var name = 'on' + inName;
-        inElement.detachEvent(name, function (e) {
-          e = e || global.event;
-          e.target = e.target || e.srcElement;
-          e.currentTarget = node;
-          inCallback.call(node, e);
-        });
-      };
-    }
-  })();
-
-  var NxDomEvent = nx.declare('nx.DomEvent', {
-    statics: {
-      on: function () {
-        var target = arguments[0];
-        var onFn = target.on;
-        if (typeof onFn === FUNCTION) {
-          return onFn.call.apply(onFn, arguments);
-        } else {
-          var args = nx.slice(arguments);
-          var context = args[0];
-          addEventListener.apply(context, args);
-          return {
-            destroy: function () {
-              return removeEventListener.apply(context, args);
-            }
-          };
-        }
-      }
-    }
-  });
-
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = NxDomEvent;
+const addEventListener = (function () {
+  if (document.addEventListener) {
+    return function (inElement, inName, inCallback, inCapture) {
+      inElement.addEventListener(inName, inCallback, inCapture || false);
+    };
+  } else if (document.attachEvent) {
+    return function (inElement, inName, inCallback) {
+      const name = 'on' + inName;
+      inElement.attachEvent(name, function (e) {
+        e = e || global.event;
+        e.target = e.target || e.srcElement;
+        e.currentTarget = node;
+        inCallback.call(node, e);
+      });
+    };
   }
 })();
+
+const removeEventListener = (function () {
+  if (document.removeEventListener) {
+    return function (inElement, inName, inCallback, inCapture) {
+      inElement.removeEventListener(inName, inCallback, inCapture || false);
+    };
+  } else if (document.detachEvent) {
+    return function (inElement, inName, inCallback) {
+      var name = 'on' + inName;
+      inElement.detachEvent(name, function (e) {
+        e = e || global.event;
+        e.target = e.target || e.srcElement;
+        e.currentTarget = node;
+        inCallback.call(node, e);
+      });
+    };
+  }
+})();
+
+var NxDomEvent = nx.declare('nx.DomEvent', {
+  statics: {
+    on: function () {
+      var target = arguments[0];
+      var onFn = target.on;
+      if (typeof onFn === FUNCTION) {
+        return onFn.call.apply(onFn, arguments);
+      } else {
+        var args = nx.slice(arguments);
+        var context = args[0];
+        addEventListener.apply(context, args);
+        return {
+          destroy: function () {
+            return removeEventListener.apply(context, args);
+          }
+        };
+      }
+    }
+  }
+});
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = NxNxDomEvent;
+}
+
+export default NxNxDomEvent;
